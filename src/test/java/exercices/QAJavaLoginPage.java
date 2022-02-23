abb4ed087123df016b00d0bbf990ac73f62678d1
Julián Hernández <julian.hernandez@electronicid.eu>
2022-02-23 14:55:01 +0000

    Update QAJavaLoginPage.java

--- src/test/java/exercices/QAJavaLoginPage.java
+++ src/test/java/exercices/QAJavaLoginPage.java
@@ -1,5 +1,17 @@
 package exercices;
 
+/*
+Please, imaging that we have a login page with two input fields (username and password) and submit button.
+
+  -Username and password fields are present
+  -Button is present
+  -If user is logged a message box is shown “Welcome to eID”
+  -If credentials are not valid, a message box is shown “You shall not pass!”
+
+  Goal 1: identify some tests to validate this login workflow
+  Goal 2: write some test in the test class below ensuring following topics:
+*/
+
 class Solution extends WebDriverTestCase {
 
     /*
